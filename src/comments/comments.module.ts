4f71a05e6d964fac3f654cf5382f612b0e5f4d3e
moisemb <papemoussa.mbengue.11@gmail.com>
2026-03-31 11:52:58 +0000

    register CommentsService provider in module

--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Comment, CommentSchema } from './schemas/comment.schema';
 import { CommentsController } from './comments.controller';
+import { CommentsService } from './comments.service';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: Comment.name, schema: CommentSchema }])],
-  controllers: [CommentsController]
+  controllers: [CommentsController],
+  providers: [CommentsService]
 })
-export class CommentsModule {}
+export class CommentsModule { }
