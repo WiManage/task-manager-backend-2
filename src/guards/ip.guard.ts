179424867254e8afd4bb2cff5a124e54a58bf58d
moisemb <papemoussa.mbengue.11@gmail.com>
2026-03-31 12:03:46 +0000

    extract allowed IPs to shared constants

--- src/guards/ip.guard.ts
+++ src/guards/ip.guard.ts
@@ -1,9 +1,11 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { ALLOWED_IPS } from '../shared/constants/allowed-ips';
 
 @Injectable()
 export class IpGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
     const request = context.switchToHttp().getRequest();
-    return request.ip === '192.168.1.255';
+
+    return ALLOWED_IPS.includes(request.ip);
   }
 }
